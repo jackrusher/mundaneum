511725704ab84f9a88387a62e4b0a2f07d6826a0
Jack Rusher <jack@rusher.com>
2019-10-09 18:07:13 +0200

    Fix loading of properties from file in resources

--- src/mundaneum/properties.clj
+++ src/mundaneum/properties.clj
@@ -1,11 +1,12 @@
 (ns mundaneum.properties
-  (:require [clojure.string :as string]
+  (:require [clojure.java.io :as io]
+            [clojure.string :as string]
             [clojure.data.json :as json]))
 
 ;; Properties fetched from:
 ;; from http://quarry.wmflabs.org/run/45013/output/1/json
 (def properties
-  (->> (json/read (clojure.java.io/reader "resources/props-2019-08-18.json") :key-fn keyword)
+  (->> (json/read (io/reader (io/resource "props-2019-08-18.json")) :key-fn keyword)
        :rows
        (reduce (fn [m [id kw]]
                  (assoc m
