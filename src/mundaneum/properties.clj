04cf446c63366bcb2c8b66ec0afab134bdbb5e6e
Gregor Middell <gregor@middell.net>
2023-05-08 11:01:09 +0200

    Updates WDT properties

--- src/mundaneum/properties.clj
+++ src/mundaneum/properties.clj
@@ -5,10 +5,12 @@
 
 ;; Properties fetched using the wikibase command line tool:
 ;; https://github.com/maxlath/wikibase-cli
-;; ... example invocation:
+;; ... example invocations:
 ;; $ wb props > props-2021-11-04.json
+;; (or via Docker):
+;; $ docker run --rm -it maxlath/wikibase-cli props -e https://query.wikidata.org/sparql >props-2023-05-08.json
 (def wdt
-  (->> (json/read (io/reader (io/resource "props-2022-04-19.json")))
+  (->> (json/read (io/reader (io/resource "props-2023-05-08.json")))
        (reduce (fn [m [id text]]
                  (assoc m
                         (-> text
